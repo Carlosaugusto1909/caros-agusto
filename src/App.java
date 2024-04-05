2f8c85d631a41a4a64bffae4df0fdff7f9eed7ad
carlosaugustodasilva <terapeutacarlos@outlook.com>
2024-04-04 21:11:30 -0300

    primeirocomiti

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.printf("dia do inter");
+        // ss_sustitui-no-texto-valor-string
+        System.out.printf("imprimi texto ");
+        //Ss subituiu-o-coringa-%d-por-um-numero-inteiro-de-texto
+        System.out.printf("minha idade é:,35");
+    }   //Ss subtitui-coringa-pro-numero-flutuante
+        System.out.printf("no valor de pi é:x;3.1414f")
+        //%. 2f-subtitui-o-coringa-%.2f.por.um.numero-flutuante
+        //com-duas-casas-decimais
+        System.out.printf(\"no valor de pi é:%.2f,3.3.1415")
+        //%b.susbtitui-coring %B Por uma valor booleano
+        System.out.printf("\n esse valor é xb".true);
+       
+}
